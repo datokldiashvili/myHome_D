2dbffad19fe343d79d98bc125f784553c3d86c5c
datokldiashvili <davitkldiashvili2007@gmail.com>
2024-01-24 20:31:56 +0400

    first commit

---
+++ API/-- SQLite.sql
@@ -0,0 +1,7 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Dato");
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Gio");
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Luka");
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+         private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        
+    }
+}
