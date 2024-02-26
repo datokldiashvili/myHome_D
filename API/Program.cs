38b3ecc0aebd059604dff3ab0c5c6cb09fd77dcf
datokldiashvili <davitkldiashvili2007@gmail.com>
2024-02-26 21:13:47 +0400

    New Details

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
-using System.Text;
+using System.Data;
+using System.Net.Security;
+using API.Data;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,11 +15,27 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "მოხდა შეცდომა მიგრაციებისას");
+}
 
 app.Run();
